49bff30f4d5ad78498dc08ca1673e01f62c6f9e5
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-10 13:46:38 +0530

    feat: Addin Base class to database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from dotenv import load_dotenv
 import os
+from sqlalchemy.orm import DeclarativeBase
 
 
 load_dotenv()
 
 engine = create_async_engine(url = os.getenv("DATABASE_URL"), echo=True)
+
+class Base(DeclarativeBase):
+    pass
