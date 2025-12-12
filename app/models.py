edd408c9eb8b043201ee10986eb0dcb4ce7eca12
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-12 12:17:08 +0530

    feat: update class Note

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from app.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Note(Base):
     __tablename__ = "notes"
-)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
