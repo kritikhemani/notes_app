43fdf5aeeb26ffff380b7b78de28f35f11706816
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-13 13:23:45 +0530

    feat: Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 
+from app.database import Base   
+from app.models import Note
+target_metadata = Base.metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
