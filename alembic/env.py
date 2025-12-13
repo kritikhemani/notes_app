98fdb0ad5eab1bfcc7ba67fe3ecf363621fb4054
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-13 13:37:52 +0530

    feat: Update alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,6 @@ from alembic import context
 
 from app.database import Base   
 from app.models import Note
-target_metadata = Base.metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
